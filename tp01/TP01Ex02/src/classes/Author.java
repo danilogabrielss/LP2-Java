0316f03995d04af593d815ae81d92c4e257b9def
Danilo Gabriel <danilogabrielss15@gmail.com>
2021-11-27 14:04:19 -0300

    tp03 completo

--- tp01/TP01Ex02/src/classes/Author.java
+++ tp01/TP01Ex02/src/classes/Author.java
@@ -2,40 +2,40 @@ package classes;
 
 public class Author {
 
-	private String name;
-	private String email;
-	private char gender;
-	
-	public Author() {
-	}
+    private String name;
+    private String email;
+    private char gender;
 
-	public Author(String name, String email, char gender) {
-		this.name = name;
-		this.email = email;
-		this.gender = gender;
-	}
+    public Author() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public char getGender() {
-		return gender;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 }
